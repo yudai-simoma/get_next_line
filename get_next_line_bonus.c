08a2abf79705e2e40580688bdf4fde2427b9d94a
yudai-shimoma <yshimoma@student.42tokyo.jp>
2023-02-19 11:00:15 +0900

    レビュー時の指摘修正

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 19:55:22 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/02/16 21:40:46 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/02/19 10:54:26 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char	*ft_strchr(const char *s, int c)
  * return_strに値があれば、joinで結合し、改行後を'\0'埋めする
  * return_strに値が無ければ、dupでbufと同じ文字列を作成し改行後を'\0'埋めする
  */
-static char	*ft_add_str(char *buf, char *return_str, size_t l_num)
+static char	*set_str_in_line(char *buf, char *return_str, size_t l_num)
 {
 	char	*cp_str;
 
@@ -64,7 +64,7 @@ static char	*ft_add_str(char *buf, char *return_str, size_t l_num)
  * return_strにbufの改行までを入れて、返す(ft_add_strで処理をする)
  * bufの改行後を先頭に移動させる
  */
-static char	*ft_buf_in_line(char *buf, char *return_str)
+static char	*buf_in_line(char *buf, char *return_str)
 {
 	size_t	i;
 	size_t	buf_size;
@@ -74,7 +74,7 @@ static char	*ft_buf_in_line(char *buf, char *return_str)
 	while (buf[i] != '\n')
 		i++;
 	i++;
-	return_str = ft_add_str(buf, return_str, i);
+	return_str = set_str_in_line(buf, return_str, i);
 	if (return_str == NULL)
 		return (NULL);
 	if (buf[i] != '\0')
@@ -93,13 +93,13 @@ static char	*ft_buf_in_line(char *buf, char *return_str)
  * bufに改行がなく、return_strに値があった場合はreturn_strとbufを結合
  * bufに改行がなく、return_strに値が無い場合は、bufの文字列をreturn_strに格納
  */
-static int	ft_while_loop(char *buf, char **return_str)
+static int	set_str(char *buf, char **return_str)
 {
 	char	*cp_str;
 
 	if (ft_strchr(buf, '\n') != NULL)
 	{
-		*return_str = ft_buf_in_line(buf, *return_str);
+		*return_str = buf_in_line(buf, *return_str);
 		return (1);
 	}
 	else
@@ -129,20 +129,26 @@ static int	ft_while_loop(char *buf, char **return_str)
  */
 char	*get_next_line(int fd)
 {
-	int			bytes;
+	ssize_t		bytes;
 	static char	buf[OPEN_MAX + 1][BUFFER_SIZE + 1];
 	char		*return_str;
 
-	if (fd >= OPEN_MAX + 1 || fd < 0 || BUFFER_SIZE < 0)
+	if (fd >= OPEN_MAX + 1 || fd < 0
+		|| BUFFER_SIZE < 0 || BUFFER_SIZE > SIZE_MAX)
 		return (NULL);
 	return_str = NULL;
 	while (1)
 	{
-		if ((buf[fd][0] != '\0') && ft_while_loop(buf[fd], &return_str))
+		if ((buf[fd][0] != '\0') && set_str(buf[fd], &return_str))
 			return (return_str);
 		bytes = read(fd, buf[fd], BUFFER_SIZE);
 		if (bytes <= 0)
 		{
+			if (bytes < 0)
+			{
+				free (return_str);
+				return_str = NULL;
+			}
 			buf[fd][0] = '\0';
 			return (return_str);
 		}
